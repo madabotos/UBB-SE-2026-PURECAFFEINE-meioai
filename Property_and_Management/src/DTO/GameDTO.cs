ed97434037fbc08eef5106d03c263170d58a2088
dianatimis24 <timis24diana@gmail.com>
2026-03-24 11:43:25 +0200

    Games - Backend

---
+++ Property_and_Management/src/DTO/GameDTO.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Property_and_Management.src.Interface;
+using Property_and_Management.src.Model;
+
+namespace Property_and_Management.src.DTO
+{
+    internal class GameDTO : IDTO<Game>
+    {
+        public int Id { get; set; }
+        public User Owner { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int MinimumPlayerNumber { get; set; }
+        public int MaximumPlayerNumber { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        public bool IsActive { get; set; }
+
+        public GameDTO(int id, User owner, string name,
+            double price, int minimumPlayerNumber, int maximumPlayerNumaber,
+            string description, byte[] image, bool isActive)
+        {
+            Id = id;
+            Owner = owner;
+            Name = name;
+            Price = price;
+            MinimumPlayerNumber = minimumPlayerNumber;
+            MaximumPlayerNumber = maximumPlayerNumaber;
+            Description = description;
+            Image = image;
+            IsActive = isActive;
+        }
+
+        public GameDTO(Game game)
+        {
+            if (game == null) throw new ArgumentNullException(nameof(game));
+
+            Id = game.Id;
+            Owner = game.Owner;
+            Name = game.Name;
+            Price = game.Price;
+            MinimumPlayerNumber = game.MinimumPlayerNumber;
+            MaximumPlayerNumber = game.MaximumPlayerNumber;
+            Description = game.Description;
+            Image = game.Image;
+            IsActive = game.IsActive;
+        }
+
+        public Game ToModel()
+        {
+            return new Game(Id, Owner, Name, Price, MinimumPlayerNumber,
+                MaximumPlayerNumber, Description, Image, IsActive);
+        }
+
+        public static IDTO<Game> FromModel(Game model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+
+            return new GameDTO(model);
+        }
+    }
+}
---
+++ Property_and_Management/src/Service/GameService.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Property_and_Management.src.DTO;
+using Property_and_Management.src.Interface;
+using Property_and_Management.src.Model;
+using Property_and_Management.src.Repository;
+
+namespace Property_and_Management.src.Service
+{
+    internal class GameService
+    {
+        private IGameRepository _gameRepository;
+
+        public GameService(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public void SetGameRepository(IGameRepository gameRepository) =>
+            _gameRepository = gameRepository;
+
+        public void AddGame(GameDTO game)
+        {
+            _gameRepository.Add(game.ToModel());
+        }
+
+        public void UpdateGameById(int id, GameDTO game)
+        {
+            _gameRepository.Update(id, game.ToModel());
+        }
+
+        public GameDTO DeleteGameById(int id)
+        {
+            return (GameDTO)GameDTO.FromModel(_gameRepository.Delete(id));
+        }
+
+        public GameDTO GetGameById(int id)
+        {
+            return (GameDTO)GameDTO.FromModel(_gameRepository.Get(id));
+        }
+
+        public ImmutableList<GameDTO> GetGamesForOwner(int ownerId)
+        {
+            return _gameRepository
+                .GetGamesByOwner(ownerId)
+                .Select(game => new GameDTO(game))
+                .ToImmutableList();
+        }
+    }
+}
