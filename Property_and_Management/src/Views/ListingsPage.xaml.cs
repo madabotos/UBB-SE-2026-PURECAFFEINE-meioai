f999c46af8045a3fe69ad8acc18077e99716101e
TaschinaLuca <taschina529@gmail.com>
2026-03-25 21:48:44 +0200

    5.6

--- Property_and_Management/src/Views/ListingsPage.xaml.cs
+++ Property_and_Management/src/Views/ListingsPage.xaml.cs
@@ -1,8 +1,8 @@
 using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.UI.Xaml.Navigation;
 using Property_and_Management.src.DTO;
 using Property_and_Management.src.Interface;
 using Property_and_Management.src.Service;
