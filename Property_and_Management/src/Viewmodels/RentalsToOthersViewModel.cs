7ef185e4eeb92a0dfca2d56ca659b5cc21809219
dariusturcu22 <protdm22@gmail.com>
2026-03-25 15:20:15 +0200

    added RentalsToOthersViewModel and corrected requirement comments

--- Property_and_Management/src/Viewmodels/RentalsToOthersViewModel.cs
+++ Property_and_Management/src/Viewmodels/RentalsToOthersViewModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Property_and_Management.src.DTO;
@@ -10,14 +9,14 @@ using Property_and_Management.src.Interface;
 
 namespace Property_and_Management.src.Viewmodels
 {
-    public class RentalsFromOthersViewModel : INotifyPropertyChanged
+    public class RentalsToOthersViewModel : INotifyPropertyChanged
     {
         private readonly IRentalService _rentalService;
         private ObservableCollection<RentalDTO> _rentals = new();
         private ObservableCollection<RentalDTO> _pagedRentals = new();
         private ImmutableList<RentalDTO> _allRentals = ImmutableList<RentalDTO>.Empty;
 
-        public int OwnerId { get; private set; } = (App.Current as App).CurrentUserID;
+        public int RenterId { get; private set; } = (App.Current as App).CurrentUserID;
 
         private const int s_pageSizeConst = 3;
         public static int PageSize => s_pageSizeConst;
@@ -73,7 +72,7 @@ namespace Property_and_Management.src.Viewmodels
 
         public string ShowingText => $"Showing {DisplayedCount} of {TotalCount} rentals";
 
-        public RentalsFromOthersViewModel(IRentalService rentalService)
+        public RentalsToOthersViewModel(IRentalService rentalService)
         {
             _rentalService = rentalService;
             LoadRentals(1, PageSize);
@@ -82,8 +81,8 @@ namespace Property_and_Management.src.Viewmodels
         // [REQ-REN-02] As a User, I must be able to see the rentals in decreasing order of the start date of the rental.
         public void LoadRentals(int page, int pageSize)
         {
-            OwnerId = (App.Current as App).CurrentUserID;
-            var allRentals = _rentalService.GetRentalsForOwner(OwnerId)
+            RenterId = (App.Current as App).CurrentUserID;
+            var allRentals = _rentalService.GetRentalsForRenter(RenterId)
                 .OrderByDescending(r => r.StartDate)
                 .ToImmutableList();
 
