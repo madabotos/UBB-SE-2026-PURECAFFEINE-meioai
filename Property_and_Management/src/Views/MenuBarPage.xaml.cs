1eb4ef1ef1a787299f2ea96aa5356a215a84f336
waytoounoriginal <mihai.tira@yahoo.ro>
2026-03-26 22:18:44 +0200

    formatter

--- Property_and_Management/src/Views/MenuBarPage.xaml.cs
+++ Property_and_Management/src/Views/MenuBarPage.xaml.cs
@@ -1,6 +1,6 @@
 using System;
-using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
