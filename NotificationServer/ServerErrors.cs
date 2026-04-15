e7f4ed8ef8c422fc2e840f4c2a01d5f928b9edb5
bbeatricecretu <109287000+bbeatricecretu@users.noreply.github.com>
2026-04-15 20:40:41 +0300

    remove things

--- NotificationServer/ServerErrors.cs
+++ NotificationServer/ServerErrors.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace NotificationServer
 {
     internal enum ServerErrors
     {
-        SERVER_NO_ERROR = 0,
-        SERVER_FAILED_INIT = -1,
+        None = 0,
+        FailedToInitializeServer = -1,
     }
 }
